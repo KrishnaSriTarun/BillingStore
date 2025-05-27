c11c493aa2d4a9f526beb91ab2530c9a79489590
Krishna <krishnasritarun95@gmail.com>
2025-05-27 07:01:50 +0530

    explore UI updated

--- frontend/src/Components/CustomerForm/CustomerForm.jsx
+++ frontend/src/Components/CustomerForm/CustomerForm.jsx
@@ -1,7 +1,20 @@
 import './CustomerForm.css'
 function CustomerForm() {
       return (
-            <div>Customer Form</div>
+            <div className="p-3">
+                  <div className="mb-3">
+                        <div className="d-flex align-items-center gap-2">
+                              <label htmlFor="customerName" className='col-4'>Customer name</label>
+                              <input type="text" className='form-control form-control-sm' id="customerName" />
+                        </div>
+                  </div>
+                  <div className="mb-3">
+                        <div className="d-flex align-items-center gap-2">
+                              <label htmlFor="mobileNumber" className='col-4'>Mobile number</label>
+                              <input type="text" className='form-control form-control-sm' id="mobileNumber" />
+                        </div>
+                  </div>
+            </div>
       );
 }
 
--- frontend/src/Components/DisplayCategory/DisplayCategory.jsx
+++ frontend/src/Components/DisplayCategory/DisplayCategory.jsx
@@ -4,7 +4,7 @@ function DisplayCategory({categories,selectedCategory,setSelectedCategory}) {
       return (
             <div className="row g-3" style={{width:'100%',margin:0}}>
                   {categories.map((category) => (
-                        <div className="col-md-3 col-sm-6" key={category.categoryId} style={{padding: '0,10px'}}>
+                        <div className="col-md-4 col-sm-6" key={category.categoryId} style={{padding: '0,15px'}}>
                               <Category categoryName={category.name} imgUrl={category.imgUrl}
                               numberOfItems={category.items} bgColor={category.bgColor}
                               isSelected={selectedCategory===category.categoryId}
